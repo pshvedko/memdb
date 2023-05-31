3f3c2158319bd862c71e7bf1f5a608016c80d5ce
Pavel Shvedko <pshvedko@gmail.com>
2023-05-31 03:30:16 +0300

    +1

--- collection.go
+++ collection.go
@@ -50,60 +50,78 @@ type Row struct {
 	sync.RWMutex
 }
 
+func (r *Row) committed() bool {
+	r.RLock()
+	defer r.RUnlock()
+	return r.cas > 0
+}
+
 type Rollback struct {
 	key   string
 	index Index
 }
 
-func (c *Collection) Put(item Item) (uint64, bool) {
+func (c Collection) Put(item Item) (uint64, bool) {
 	one := &Row{}
 	one.Lock()
 	defer one.Unlock()
+index:
 	row, key, ok := c.Indexes[0].Put(c.Indexes[0].Key(item), one)
 	if ok {
-		return c.update(row, item)
+		if row.committed() {
+			return c.update(row, item)
+		}
+		goto index
 	}
 	return c.insert(row, item, Rollback{index: c.Indexes[0], key: key})
 }
 
-func (c *Collection) update(row *Row, item Item) (uint64, bool) {
+func (c Collection) update(row *Row, item Item) (uint64, bool) {
 	row.Lock()
 	defer row.Unlock()
 	var rollbacks, unleashes []Rollback
 	for _, index := range c.Indexes[1:] {
+	index:
 		one, key, ok := index.Put(index.Key(item), row)
 		if ok {
 			if one != row {
-				return rollback(rollbacks...)
+				if one.committed() {
+					return c.rollback(rollbacks...)
+				}
+				goto index
 			}
 			continue
 		}
 		rollbacks = append(rollbacks, Rollback{index: index, key: key})
 		unleashes = append(unleashes, Rollback{index: index, key: index.Key(row)})
 	}
-	return commit(row, item, unleashes...)
+	return c.commit(row, item, unleashes...)
 }
 
-func (c *Collection) insert(row *Row, item Item, rollbacks ...Rollback) (uint64, bool) {
+func (c Collection) insert(row *Row, item Item, rollbacks ...Rollback) (uint64, bool) {
 	for _, index := range c.Indexes[1:] {
-		_, key, ok := index.Put(index.Key(item), row)
+	index:
+		one, key, ok := index.Put(index.Key(item), row)
 		if ok {
-			return rollback(rollbacks...)
+			if one.committed() {
+				return c.rollback(rollbacks...)
+			}
+			goto index
 		}
 		rollbacks = append(rollbacks, Rollback{key: key, index: index})
 	}
-	return commit(row, item)
+	return c.commit(row, item)
 }
 
-func rollback(rollbacks ...Rollback) (uint64, bool) {
+func (c Collection) rollback(rollbacks ...Rollback) (uint64, bool) {
 	for _, r := range rollbacks {
 		r.index.Delete(r.key)
 	}
 	return 0, false
 }
 
-func commit(row *Row, item Item, rollbacks ...Rollback) (uint64, bool) {
-	rollback(rollbacks...)
+func (c Collection) commit(row *Row, item Item, rollbacks ...Rollback) (uint64, bool) {
+	c.rollback(rollbacks...)
 	row.Item = item
 	row.cas++
 	return row.cas, true
